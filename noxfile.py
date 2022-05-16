e7f7d5bc5fc0c947afa7d30a8a3a4882274b9acc
Frost Ming <mianghong@gmail.com>
2022-05-16 16:14:36 +0800

    pdm use venv

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import os
 
 import nox
 
-os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
+os.environ.update(PDM_IGNORE_SAVED_PYTHON="1", PDM_USE_VENV="1")
 
 
 @nox.session(python=("3.7", "3.8", "3.9", "3.10"))
