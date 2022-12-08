f2a027c154d09ef2703def386cdd52d28f035620
Frost Ming <mianghong@gmail.com>
2022-12-08 10:15:28 +0800

    doc: update the version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Frost Ming"
 author = "Frost Ming"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.2"
+release = "0.6.3"
 
 
 # -- General configuration ---------------------------------------------------
